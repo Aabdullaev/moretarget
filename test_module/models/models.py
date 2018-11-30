b5b471a3aea4ca3f8e7c1d4df3ae13bc93c87803
Phuc Le <phucle>
2018-11-30 11:12:29 +0700

    complete module test_module

--- test_module/models/models.py
+++ test_module/models/models.py
@@ -2,6 +2,28 @@
 
 from odoo import models, fields, api
 
+
+class Course1(models.Model):
+    _name = 'test_module.course'
+    _description = "test_module.course description"
+
+    description = fields.Text()
+    name = fields.Text()
+
+
+class Session1(models.Model):
+    _name = 'test_module.session'
+    _description = "OpenAcademy Sessions"
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
+    # course_id = fields.Many2one('test_module.course',ondelete='cascade', string="Course", required=True)
+    test_module_id = fields.Many2one("test_module.test_module", string="Test Module")
+    course_ids = fields.Many2many('test_module.course', string="Attendee")
+
+
 class test_module(models.Model):
     _name = 'test_module.test_module'
 
@@ -9,6 +31,8 @@ class test_module(models.Model):
     value = fields.Integer()
     value2 = fields.Float(compute="_value_pc", store=True)
     description = fields.Text()
+    responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True)
+    session_ids = fields.One2many('test_module.session', 'test_module_id', string="Sessions")
 
     @api.depends('value')
     def _value_pc(self):
